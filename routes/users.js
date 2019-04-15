8e650493a37372ea72fccb7a914024263aea2710
Ana Sabau <ana.m.sabau@gmail.com>
2019-04-15 21:44:41 +0300

    push person

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -12,6 +14,17 @@ router.post('/add', function(req, res, next) {
   var lastName = req.body.lastName;
   var phone = req.body.phone;
   console.warn ('add', firstName, lastName, phone);
+  var persons = require('../public/data/persons.json');
+  persons.push({
+    firstName,
+    lastName,
+    phone
+  });
+  //firstName echivalent cu firstName: firstName
+
+  var str = JSON.stringify(persons, null, 2);
+  fs.writeFileSync('./public/data/persons.json', str);
+
   res.json({
     success: true,
     message: 'TODO'
