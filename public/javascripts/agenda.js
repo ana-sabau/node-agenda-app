636f6d40e7943df4cc64bf25f022f738f65d1c64
Ana Sabau <ana.m.sabau@gmail.com>
2019-05-06 21:10:27 +0300

    add and delete person

--- public/javascripts/agenda.js
+++ public/javascripts/agenda.js
@@ -3,30 +3,35 @@ var allPersons = [];
 
 var API_URL = {
     //ADD: 'data/add.json'
-    ADD: 'users/add'
+    ADD: 'users/add',
+    DELETE: 'users/delete'
 };
 
 var API_METHOD = {
     //ADD: 'GET'
-    ADD: 'POST'
+    ADD: 'POST',
+    DELETE: 'DELETE'
 };
 
-fetch('data/persons.json').then(function(r){
+fetch('data/persons.json').then(function (r) {
     return r.json();
-}).then(function(persons){
+}).then(function (persons) {
     console.log('all persons', persons);
     allPersons = persons;
     display(persons);
 });
 
 function display(persons) {
-    var list = persons.map(function(person){
-        return `<tr>
-                                <td>${person.firstName}</td>
-                                <td>${person.lastName}</td>
-                                <td>${person.phone}</td>
-                                <td></td>
-                            </tr>`;
+    var list = persons.map(function (person) {
+        return `<tr data-id="${person.id}">
+                    <td>${person.firstName}</td>
+                    <td>${person.lastName}</td>
+                    <td>${person.phone}</td>
+                    <td>
+                    <a href="#"class="delete">&#10006;</a>
+                    <a href="#"class="edit">&#9998;</a>
+                    </td>
+                </tr>`;
     });
     document.querySelector('#agenda tbody').innerHTML = list.join('');
 }
@@ -39,7 +44,6 @@ function savePerson() {
 }
 
 function submitNewPerson(firstName, lastName, phone) {
-    console.warn('submitNewPerson', firstName, lastName, phone);
     var body = null;
     if (API_METHOD.ADD === 'POST') {
         body = JSON.stringify({
@@ -48,15 +52,16 @@ function submitNewPerson(firstName, lastName, phone) {
             phone: phone
         });
     }
+
     fetch(API_URL.ADD, {
         method: API_METHOD.ADD,
         body: body,
         headers: {
-            "Content-Type": "applications/json"
+            "Content-Type": "application/json"
         }
-    }).then(function(r){
+    }).then(function (r) {
         return r.json();
-    }).then(function(status){
+    }).then(function (status) {
         if (status.success) {
             inlineAddPerson(firstName, lastName, phone);
         }
@@ -74,3 +79,47 @@ function inlineAddPerson(firstName, lastName, phone) {
     });
     display(allPersons);
 }
+
+function inlineDeletePerson(id){
+    console.warn('please refresh', id);
+    allPersons = allPersons.filter(function(person){
+        return person.id != id;
+    });
+    display(allPersons);
+}
+
+function deletePerson(id) {
+    var body = null;
+    if (API_METHOD.DELETE === 'DELETE') {
+        body = JSON.stringify({id});
+    }
+    fetch(API_URL.DELETE, {
+        method: API_METHOD.DELETE,
+        body: body,
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }).then(function (r) {
+        return r.json();
+    }).then(function (status) {
+        if (status.success) {
+            inlineDeletePerson(id);
+        }
+        else {
+            console.warn('not removed', status);
+        }
+    })
+}
+
+function initEvents() {
+    const tbody = document.querySelector('#agenda tbody');
+    tbody.addEventListener('click', function(e){
+        if (e.target.className == 'delete'){
+            const tr = e.target.parentNode.parentNode;
+            const id = tr.getAttribute('data-id');
+            deletePerson(id);
+        }
+    });
+}
+
+initEvents();
--- routes/users.js
+++ routes/users.js
@@ -13,21 +13,51 @@ router.post('/add', function(req, res, next) {
   var firstName = req.body.firstName;
   var lastName = req.body.lastName;
   var phone = req.body.phone;
-  console.warn ('add', firstName, lastName, phone);
-  var persons = require('../public/data/persons.json');
+  console.warn ('add', firstName, lastName, phone, req.body);
+  
+  var persons = require('../public/data/persons.json'); //fisierul e la 2 nivele in sus fata de folderul curent
+  //echivalent cu:
+  //var strPersons = fs.readFileSync('./public/data/persons.json');
+  //var persons = JSON.parse(strPersons);
+
   persons.push({
     firstName,
     lastName,
     phone
   });
-  //firstName echivalent cu firstName: firstName
+  //firstName echivalent cu firstName: firstName; doar daca cheia e egala cu valoarea
+  //pana aici persoana noua apare doar in memorie
 
   var str = JSON.stringify(persons, null, 2);
-  fs.writeFileSync('./public/data/persons.json', str);
+
+  console.log('str', str);
+
+  fs.writeFileSync('./public/data/persons.json', str); //fs.write citeste de la nivelul principal al proiectului, de aceea e ./public
 
   res.json({
     success: true,
-    message: 'TODO'
+    message: 'Done'
+  });
+});
+
+router.delete('/delete', function(req, res, next) {
+  var id = req.body.id;
+  console.warn('remove person', id);
+  
+  var persons = require('../public/data/persons.json'); 
+
+  var remainingPersons = persons.filter(function(person){
+    return person.id != id; //in lista finala trebuie sa ramana persoanele care au id diferit de id-ul pe care vrem sa il stergem
+  });
+  //functia filter returneaza un alt array
+
+  var str = JSON.stringify(remainingPersons, null, 2);
+
+  fs.writeFileSync('./public/data/persons.json', str); 
+
+  res.json({
+    success: true,
+    message: 'Done'
   });
 });
 
